af95269b3b6fd201bb570a24b9edd2372d4faf43
Said Mohamed <warstander45@gmail.com>
2024-10-10 09:45:28 -0400

    Title change App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 100997363 || Said Mohamed || DevOps</h5>
+      <h5>Developed By: 100997363 || Said Mohamed || DevOps - COMP3104</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
